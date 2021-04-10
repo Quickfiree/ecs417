3b8615e52486baedf5e631371e0f5c50be643a1b
Quickfiree <jatin.01@hotmail.com>
2021-04-10 15:21:19 +0100

    tried something even simpler

--- webroot/portfolio/testindex.php
+++ webroot/portfolio/testindex.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> Test site </title>
-    </head>
-    <body>
-        <?php
-        echo "Hello world!";
-        ?>
-    </body>
-</html>
+<?php
+echo "Hello world!";
+?>
