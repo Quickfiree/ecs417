bfdda71543c5ee91d74804e16d2cb58a8848c4d5
Quickfiree <jatin.01@hotmail.com>
2021-04-12 16:45:48 +0100

    fixed sql query

--- webroot/portfolio/uploadpost.php
+++ webroot/portfolio/uploadpost.php
@@ -2,8 +2,9 @@
     session_start();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $sql = "INSERT INTO BLOGPOSTS
+        $sql = "INSERT INTO BLOGPOSTS (postTitle, postBody, postDate)
         VALUES ('$title', '$body', date('Y-m-d H:i:s'))";
+
         if ($conn -> query($sql) === TRUE) {
             echo "Post added! Redirecting...";
             //header("location:http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/viewblog.php");
--- webroot/portfolio/viewblog.php
+++ webroot/portfolio/viewblog.php
@@ -1,6 +1,90 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title> View blog posts </title>
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "projects.css">
+    <title>Projects</title>
+</head>
+<body>
+        <?php
+            session_start();
+        ?>
+        <script>
+            function showLogOut() {
+                document.getElementById("loginButton").innerHTML = '<a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/logout.php">Log Out</a>';
+            }
+
+            function showLogIn() {
+                document.getElementById("loginButton").innerHTML = '<a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/login.html">Log In</a>';
+            }
+        </script>
+        <nav>
+            <ul class = "horizontal">
+                <li>
+                    <a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/index.php">Home</a>
+                </li>
+                <li>
+                    <a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/projects.php" class = "active">Projects</a>
+                </li>
+                <li>
+                    <a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/addpost.php">Add a blog post</a>
+                </li>
+                <li>
+                    <a href = "https://www.linkedin.com/in/jatinkumar-patel-5139a8201/">Contact - LinkedIn</a>
+                </li>
+                <li id = "loginButton">
+
+                </li>
+            </ul>
+        </nav>
+        <?php
+            if (isset ($_SESSION['user'])) {
+            // Do if user is logged in
+                echo '<script type = "text/javascript">showLogOut();</script>';
+                
+            } else {
+                // Nobody is logged in
+                echo '<script type = "text/javascript">showLogIn();</script>';
+            }
+        ?>
+    <div class = "main">
+        <article>
+            <?php
+                $dbhost = getenv("MYSQL_SERVICE_HOST");
+                $dbport = getenv("MYSQL_SERVICE_PORT");
+                $dbuser = getenv("DATABASE_USER");
+                $dbpwd = getenv("DATABASE_PASSWORD");
+                $dbname = getenv("DATABASE_NAME");
+            
+                // Creates connection to database
+                $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+            
+                // Checks connection
+                if ($conn -> connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+                
+                $query = "SELECT * FROM BLOGPOSTS";
+                $res = mysqli_query($conn, $query);
+                $rows = mysqli_num_rows($res);
+
+                for ($i = 0; $i < $rows; $i++) {
+                    $title = $res
+                    # code...
+                }
+            ?>
+        </article>
+        <footer>
+            <p>
+                <a href = "http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/index.php">Home</a>
+                |
+                <a href = "https://github.com/Quickfiree/">Github</a>
+            </p>
+            <p><i>Copyright &copy; 2021 Jatinkumar Patel</i></p>
+        </footer>
+    </div>
+
+    
+</body>
 </html>
