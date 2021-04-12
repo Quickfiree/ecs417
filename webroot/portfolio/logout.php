ae28e4371c060a2324466a32e9ea3b154a551912
Quickfiree <jatin.01@hotmail.com>
2021-04-12 12:12:39 +0100

    trying to log out and end session

--- webroot/portfolio/logout.php
+++ webroot/portfolio/logout.php
@@ -1,2 +1,8 @@
 <?php
     session_start();
+    session_unset();
+    session_destroy();
+
+    header("location:http://cakephp-mysql-persistent-ecs417-jatin.apps.okd.eecs.qmul.ac.uk/portfolio/index.php");
+    exit();
+?>
