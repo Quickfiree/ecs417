254c269865accb7b90a5e408aec6e187b64965cc
Quickfiree <jatin.01@hotmail.com>
2021-04-13 03:56:30 +0100

    fixed a logical error

--- webroot/portfolio/viewblog.php
+++ webroot/portfolio/viewblog.php
@@ -53,81 +53,54 @@
         ?>
     <div class = "main">
         <article>
-            <form action = "viewblog.php" method = "POST">
+        <form action = "viewblog.php" method = "POST">
             <input type = "date" name = "date">
             <input type = "submit" value = "SORT" name = "sortDate">
         </form>
-            <?php
-                $dbhost = getenv("MYSQL_SERVICE_HOST");
-                $dbport = getenv("MYSQL_SERVICE_PORT");
-                $dbuser = getenv("DATABASE_USER");
-                $dbpwd = getenv("DATABASE_PASSWORD");
-                $dbname = getenv("DATABASE_NAME");
+        <?php
+            $dbhost = getenv("MYSQL_SERVICE_HOST");
+            $dbport = getenv("MYSQL_SERVICE_PORT");
+            $dbuser = getenv("DATABASE_USER");
+            $dbpwd = getenv("DATABASE_PASSWORD");
+            $dbname = getenv("DATABASE_NAME");
+        
+            // Creates connection to database
+            $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+        
+            // Checks connection
+            if ($conn -> connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
             
-                // Creates connection to database
-                $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-            
-                // Checks connection
-                if ($conn -> connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
+            if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $new_date = date('Y-m-d', strtotime($_POST['date']));
+                echo $new_date;
+                $query = "SELECT * FROM BLOGPOSTS WHERE postDate = $new_date";
+                $res = mysqli_query($conn, $query);
+                $rows = mysqli_num_rows($res);
+                $dateArray = array();
+
+                for ($i = 0; $i < $rows; $i++) {
+                    $row = $res->fetch_assoc();
+                    $dateArray[$i] = array("date" => $row['postDate'], "time" => $row['postTime'], $row['postTitle'], $row['postBody']);
+                }
+
+                array_multisort( // Sorts according to oldest first
+                    array_map('strtotime', array_column($dateArray, 'date')),
+                    array_column($dateArray, 'time'),
+                    $dateArray
+                );
+
+                $dateArray = array_reverse($dateArray); // Reverses the array to make the order newest first.
+
+                for ($i = 0; $i < $rows; $i++) {
+                    $title = $dateArray[$i][0];
+                    $body = $dateArray[$i][1];
+                    echo "<section><h1>$title</h1>";
+                    echo "<p>$body</p></section>";
                 }
                 
-                if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-                    $new_date = date('Y-m-d', strtotime($_POST['date']));
-                    echo $new_date;
-                    $query = "SELECT * FROM BLOGPOSTS WHERE postDate = $new_date";
-                    $res = mysqli_query($conn, $query);
-                    $rows = mysqli_num_rows($res);
-                    $dateArray = array();
-
-                    for ($i = 0; $i < $rows; $i++) {
-                        $row = $res->fetch_assoc();
-                        $dateArray[$i] = array("date" => $row['postDate'], "time" => $row['postTime'], $row['postTitle'], $row['postBody']);
-                    }
-    
-                    array_multisort( // Sorts according to oldest first
-                        array_map('strtotime', array_column($dateArray, 'date')),
-                        array_column($dateArray, 'time'),
-                        $dateArray
-                    );
-    
-                    $dateArray = array_reverse($dateArray); // Reverses the array to make the order newest first.
-    
-                    for ($i = 0; $i < $rows; $i++) {
-                        $title = $dateArray[$i][0];
-                        $body = $dateArray[$i][1];
-                        echo "<section><h1>$title</h1>";
-                        echo "<p>$body</p></section>";
-                    }
-                    
-                } else {
-                    // First get dates and sort them
-                    $sql = "SELECT * FROM BLOGPOSTS";
-                    $res = mysqli_query($conn, $query);
-                    $rows = mysqli_num_rows($res);
-                    $dateArray = array();
-
-                    for ($i = 0; $i < $rows; $i++) {
-                        $row = $res->fetch_assoc();
-                        $dateArray[$i] = array("date" => $row['postDate'], "time" => $row['postTime'], $row['postTitle'], $row['postBody']);
-                    }
-    
-                    array_multisort( // Sorts according to oldest first
-                        array_map('strtotime', array_column($dateArray, 'date')),
-                        array_column($dateArray, 'time'),
-                        $dateArray
-                    );
-    
-                    $dateArray = array_reverse($dateArray); // Reverses the array to make the order newest first.
-    
-                    for ($i = 0; $i < $rows; $i++) {
-                        $title = $dateArray[$i][0];
-                        $body = $dateArray[$i][1];
-                        echo "<section><h1>$title</h1>";
-                        echo "<p>$body</p></section>";
-                    }
-                }
-                /*
+            } else {
                 // First get dates and sort them
                 $query = "SELECT * FROM BLOGPOSTS";
                 $res = mysqli_query($conn, $query);
@@ -153,8 +126,35 @@
                     echo "<section><h1>$title</h1>";
                     echo "<p>$body</p></section>";
                 }
-                */
-            ?>
+            }
+            /*
+            // First get dates and sort them
+            $query = "SELECT * FROM BLOGPOSTS";
+            $res = mysqli_query($conn, $query);
+            $rows = mysqli_num_rows($res);
+            $dateArray = array();
+
+            for ($i = 0; $i < $rows; $i++) {
+                $row = $res->fetch_assoc();
+                $dateArray[$i] = array("date" => $row['postDate'], "time" => $row['postTime'], $row['postTitle'], $row['postBody']);
+            }
+
+            array_multisort( // Sorts according to oldest first
+                array_map('strtotime', array_column($dateArray, 'date')),
+                array_column($dateArray, 'time'),
+                $dateArray
+            );
+
+            $dateArray = array_reverse($dateArray); // Reverses the array to make the order newest first.
+
+            for ($i = 0; $i < $rows; $i++) {
+                $title = $dateArray[$i][0];
+                $body = $dateArray[$i][1];
+                echo "<section><h1>$title</h1>";
+                echo "<p>$body</p></section>";
+            }
+            */
+        ?>
         </article>
         <footer>
             <p>
