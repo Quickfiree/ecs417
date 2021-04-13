cc8c30484df722cd3af2311ee3591d6b9eec46b9
Quickfiree <jatin.01@hotmail.com>
2021-04-13 04:54:03 +0100

    more tests again

--- webroot/portfolio/viewblog.php
+++ webroot/portfolio/viewblog.php
@@ -74,8 +74,10 @@
             
             if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                 $new_date = date('Y-m-d', strtotime($_POST['date']));
-                $dateObj = DateTime::createFromFormat('jS F Y', $new_date);
-                $query = "SELECT * FROM BLOGPOSTS WHERE cast(postDate as date) = '$dateObj -> format("n")'";
+                $timestamp = strtotime($new_date);
+                $month = date("n", $timestamp);
+                echo $month;
+                $query = "SELECT * FROM BLOGPOSTS WHERE cast(postDate as date) = '$new_date'";
                 $res = mysqli_query($conn, $query);
                 $rows = mysqli_num_rows($res);
                 $dateArray = array();
